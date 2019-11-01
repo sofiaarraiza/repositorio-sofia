de4ab68747e250baf0b895312ad537ca1a438e5d
sofiaarraiza <sofia.arraiza@gmail.com>
2019-11-01 20:37:37 -0300

    ..

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
-const mongoose = require("mongoose");
-
+const modelo = require('./models/City.js');
 
 app.get("/", (req, res) => res.send("HELLO WORLD"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 app.get("/test", (req, res) => res.send("HOLA BB"));
+let uri = "mongodb+srv://desaUser:asd123@cluster0-3yxfd.mongodb.net/test?retryWrites=true&w=majority"; 
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true }).then( res => {console.log("Connected to DB")}).catch( err => {console.log("err: " + err)});
 
 
+app.get('/cities', async (req, res) => {
+
+        if (res.status(200)){
+            modelo.find( {}).then (data => {
+                console.log("datos por consola: " + data);
+                res.json(data);
+    }).catch( err => {console.log("err: " + err);});
+}
+    
+    // modelo.model('barcelona').find(function(err, barcelona){
+    //     res.send(barcelona);
+    // });
+});
+
+
+//mongodb+srv://sofiaarraiza:<password>@cluster0-3yxfd.mongodb.net/test?retryWrites=true&w=majority
 
